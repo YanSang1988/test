e48d3ff301baf3b61ec21ba841f83136dfed895a
wit <925789276@qq.com>
2020-05-05 03:06:08 +0800

    add a

--- a.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main()
-{
-		printf("hello git\n");
-		printf("\n");
-}
-
---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+		int a=10;
+		int b=20;
+		(a<b?a:b)=30;
+		printf("a=%d,b=%d\n",a,b);
+		return 0;
+}
