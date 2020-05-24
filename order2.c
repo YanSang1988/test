8fc3f656ee2aa96735aab8d3c7faf7dbd19fbdcd
wit <925789276@qq.com>
2020-05-25 01:37:38 +0800

    add order2.c

--- order2.c
+++ order2.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
+
 int spitstring(const char *buf1,char c,char ***myp3,int *count)
 {
 		int ret=0;
@@ -26,9 +27,11 @@ int spitstring(const char *buf1,char c,char ***myp3,int *count)
 						break;
 				}
 		}while(*p!='\0');
-}
-*count=tmpcount;
+
+
+*count=tmpcount;//间接赋值是指针存在的最大意义！
 myp=(char **)malloc(tmpcount*sizeof(char *));
+
 if(myp==NULL)
 {
 		ret=-1;
@@ -37,6 +40,48 @@ if(myp==NULL)
 }
 memset(myp,0,tmpcount&sizeof(char *));
 
+tmpcount=0;
+p=buf1;
+ptmp=buf1;
+
+do
+{
+		p=strchr(p,c);
+		if(p!=NULL)
+		{
+				if(p-ptmp>0)
+				{
+						int len=p-ptmp+1;
+						myp[tmpcount]=(char *)malloc(len);
+						if(myp[tmpcount]==NULL)
+						{
+								ret=-1;
+								printf("function error!\n");
+								goto END;
+						}
+						strncpy(myp[tmpcount],ptmp,p-ptmp);
+						myp[tmpcount][p-ptmp]='\0';
+						tmpcount++;
+						ptmp=p=p+1;
+				}
+		}
+		else
+		{
+				break;
+		}
+}while(*p!='\0');
+
+END:
+		if(ret!=0)
+		{
+				freemem(&myp3,*count);												
+		}
+		else
+		{
+				*myp3=myp;
+		}
+		return ret;		
+}
 
 void main()
 {
@@ -46,8 +91,8 @@ void main()
 		int ncount;
 		char **p=NULL;
 		
-		p=spitstring(p1,ctem,&p,&ncount);
-		if(p=NULL)
+		ret=spitstring(p1,ctem,&p,&ncount);
+		if(ret!=0)
 		{
 				printf("function error!\n");
 		}
